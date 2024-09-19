13cab30ce34a086b1444c62d5aed9ef2e99df771
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-09-19 13:51:58 -0300

    initial structure and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
